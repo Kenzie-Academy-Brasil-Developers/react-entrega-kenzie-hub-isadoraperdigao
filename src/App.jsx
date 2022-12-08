4f84da436aa219c2082fa618b4b3558deda97726
Isadora <dorisperdigao@gmail.com>
2022-12-08 10:57:17 -0300

    feature: creates userContex

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { DashboardPage } from './pages/dashboardPage';
 import { LoginPage } from './pages/loginPage';
 import { RegisterPage } from './pages/registerPage';
 import { Route, Routes } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<LoginPage />} />
-      <Route path='register' element={<RegisterPage />} />
-      <Route path='dashboard/:name' element={<DashboardPage />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route index element={<LoginPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        <Route path='dashboard/:name' element={<DashboardPage />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
